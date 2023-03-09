210c59759dcf36b863d7c9b9460dad71478261aa
ThangCA <nguyen_duc_thang@ca-adv.co.jp>
2023-03-09 17:29:36 +0900

    Nguyen Duc Thang _ 1911065862

--- git-C5/my-web-app/app.js
+++ git-C5/my-web-app/app.js
@@ -22,8 +22,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// Doi 
+//thay doi
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
