53f0627e84a28f2a916b6ab872bb0c6e39798a29
thangCA <112834196+thangCA@users.noreply.github.com>
2023-03-09 17:31:21 +0900

    Nguyen Duc Thang _ 1911065862

--- git-C5/my-web-app/app.js
+++ git-C5/my-web-app/app.js
@@ -7,6 +7,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// Thang sua tu CloneGitC5
+//Thang tiep tuc sua Code
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
