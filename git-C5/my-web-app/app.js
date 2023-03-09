bd3a1effb37cef64f9f9f771ce047c69c9f99c5c
ThangCA <nguyen_duc_thang@ca-adv.co.jp>
2023-03-09 16:37:10 +0900

    NguyenDucThang_1911065862

--- git-C5/my-web-app/app.js
+++ git-C5/my-web-app/app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./routes/users');
 var app = express();
 // Thang sua tu CloneGitC5
 //Thang tiep tuc sua Code
-// Sua lan n
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
